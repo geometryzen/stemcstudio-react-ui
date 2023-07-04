0a8fe23b20eee1353e713b00f55e76b872a67b1c
David Geo Holmes <geometryzen@gmail.com>
2023-07-04 11:44:52 -0400

    Initial Commit

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,14 +1,15 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import * as React from 'react';
 import { Button } from '../buttons/Button';
 
 const meta: Meta<typeof Button> = {
     title: "Example/Button",
-    component: Button,
+    component: Button
 };
 
 export default meta;
 type Story = StoryObj<typeof Button>;
 
-export const Primary: Story = {
-    render: (args) => <Button {...args} />
+export const Default: Story = {
+    render: () => <Button />
 };
