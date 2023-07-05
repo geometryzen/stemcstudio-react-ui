434f4f894559d4348f4d25720492f3a78d768cb3
David Geo Holmes <geometryzen@gmail.com>
2023-07-05 11:17:27 -0400

    Initial Commit

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -13,3 +13,19 @@ type Story = StoryObj<typeof Button>;
 export const Default: Story = {
     render: (args) => <Button {...args}>Button</Button>,
 };
+
+export const Primary: Story = {
+    render: (args) => (
+        <Button data-type="primary" {...args}>
+            Button
+        </Button>
+    ),
+};
+
+export const Secondary: Story = {
+    render: (args) => (
+        <Button data-type="secondary" {...args}>
+            Button
+        </Button>
+    ),
+};
